e1e8b14224608bfde4ab774e0b665c763760853b
TOPCMU <topsosmartandcool@gmail.com>
2024-08-23 15:27:47 +0700

    Initial commit

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,31 @@
 from app import app
 from flask import render_template
+import pymysql
+def conDB():
+    return pymysql.connect(
+            host='sql.freedb.tech',
+            port=3306,
+            user='freedb_TOPCMU',
+            password='#6apSzfG!!wjq@T',
+            database='freedb_MPP001',
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor)
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/show')
+def show():
+    conn = conDB()
+    cursor = conn.cursor()
+    # ดึงข้อมูลทั้งหมดจากตาราง test001
+    select_query = "SELECT * FROM test001"
+    cursor.execute(select_query)
+    # ดึงข้อมูลทั้งหมดจากผลลัพธ์
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    # print(rows)
+    # ส่งข้อมูลไปยังเทมเพลต
+    return render_template('show.html', rows=rows)
--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -4,13 +4,18 @@ document.addEventListener('DOMContentLoaded', function () {
     // แสดงข้อความในคอนโซลเพื่อยืนยันว่าไฟล์ JavaScript ทำงาน
     console.log('JavaScript is running!');
     const abc=document.getElementsByClassName('abc');
-    abc[0].innerHTML += '<p>12345667</p>';
+    abc[0].innerHTML += '<p>let\' fucking go</p>';
     // ฟังก์ชันสำหรับการทักทายผู้ใช้เมื่อคลิกปุ่ม
     const greetButton = document.getElementById('greetButton');
     if (greetButton) {
         greetButton.addEventListener('click', function () {
             alert('let\'s go');
-            window.location.href = 'https://youtu.be/A-9K9IpgTho?si=Lwg_SumcNt8LXHvy';
+            window.location.href = 'https://youtu.be/vOreqez4v9Y?si=cUVz-q4TX-GXKDbw';
+        });
+    }
+    if (showButton) {
+        showButton.addEventListener('click', function () {
+            window.location.href = '/show';
         });
     }
 });
